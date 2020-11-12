2d1e574671e1bb3336c6882f5730de69a6577249
Teten Nugraha <teten.nugraha@allianz.co.id>
2020-11-12 15:35:09 +0700

    first-project

---
+++ server-app/src/test/java/id/grpc/serverapp/ServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package id.grpc.serverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
