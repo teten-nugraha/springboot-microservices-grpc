930714af2e62e74330df737aea15b30e8d3d0090
Teten Nugraha <teten.nugraha@allianz.co.id>
2020-11-13 08:51:54 +0700

    update

--- client-app/src/main/java/id/grpc/clientapp/ClientAppApplication.java
+++ client-app/src/main/java/id/grpc/clientapp/ClientAppApplication.java
@@ -1,5 +1,7 @@
 package id.grpc.clientapp;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClientAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClientAppApplication.class, args);
+
+//		SpringApplication.run(ClientAppApplication.class, args);
+
+		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
+				.usePlaintext()
+				.build();
+
+		HelloServiceGrpc.HelloServiceBlockingStub stub
+				= HelloServiceGrpc.newBlockingStub(channel);
+
+		HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
+				.setFirstName("Baeldung")
+				.setLastName("gRPC")
+				.build());
+
+		System.out.println(helloResponse);
+
+		channel.shutdown();
+
+
 	}
+
 
 }
